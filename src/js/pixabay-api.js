67eab7f7ef3299eec615f455c5051b8e21e15afb
Kateryna Rudakova <ekaterinarudakova170893@gmail.com>
2026-09-08 13:35:39 +0200

    Homework 11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '57512176-00f0b64b0dbc93a3c4327f754';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+}
